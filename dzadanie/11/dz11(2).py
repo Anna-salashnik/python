7c8236c2cf0f28a642572efa17e47dff7bcebc9e
Anna Salashnik <150508857+Anna-salashnik@users.noreply.github.com>
2024-04-06 11:52:50 +0300

    Update dz11(2).py

--- dzadanie/11/dz11(2).py
+++ dzadanie/11/dz11(2).py
@@ -1,5 +1,11 @@
-from turtle import*;
-hideturtle();
-speed(6); 
-for i in range(8): circle((i+1)*10)  
-done()
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(6)
+colors = ["blue", "green", "pink", "red", "black"]  # Список цветов для смены
+
+for i in range(5):
+    turtle.color(colors[i % len(colors)])  # Меняем цвет на каждом кругу
+    turtle.circle((i + 1) * 10)
+
+turtle.done()
