bcacb47b1b4aa1cd74160ecf4b7b3bd012392664
Anna Salashnik <150508857+Anna-salashnik@users.noreply.github.com>
2024-04-13 11:37:29 +0300

    Update main3.py

--- samRabota/main3.py
+++ samRabota/main3.py
@@ -1,6 +1,7 @@
 massa = float(input("Введи массу конфет в граммах: "))
 if massa > 2000:
     chena = massa / 1000 * 200
+    print(f"200.0 руб за 1кг, Итого:{chena} руб")
 else:
     chena = massa / 1000 * 250
-print(f"Цена конфет: {chena} руб")
+    print(f"250.0 руб за 1кг, Итого:{chena} руб")
